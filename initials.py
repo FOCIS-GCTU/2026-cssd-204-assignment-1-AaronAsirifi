f4d9f44429248c927a38fed25419b562716c16ae
AaronAsirifi <aaronasirifi11@gmail.com>
2026-05-31 10:24:34 -0700

    ASSIGNMENT 1

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Prints my initials ABA in stylized large letters.
+# Assignment Number: 1
+#
+# Name: AARON BOAKYE ASIRIFI
+# STUDENT ID: 2425400618
+# Email: 2425400618@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, AARON BOAKYE ASIRIFI, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # This program prints out my initials in stylized large letters.
 
+    print()
+    print("...ABA")
+    print()
 
-
+    print("...AAAAAAAAAAAA........BBBBBBBBB...........AAAAAAAAAAAA.....")
+    print("...AA........AA........BBBBBBBBB...........AA........AA.....")
+    print("...AA........AA........BB......BB..........AA........AA.....")
+    print("...AA........AA........BB......BB..........AA........AA.....")
+    print("...AAAAAAAAAAAA........BBBBBBBBB...........AAAAAAAAAAAA.....")
+    print("...AA........AA........BB......BB..........AA........AA.....")
+    print("...AA........AA........BB......BB..........AA........AA.....")
+    print("...AA........AA........BB......BB..........AA........AA.....")
+    print("...AA........AA...**...BBBBBBBBB......**...AA........AA...**")
+    print("...AA........AA...**...BBBBBBBBB......**...AA........AA...**")
+    print()
+    
 main()
